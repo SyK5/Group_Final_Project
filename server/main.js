7f696aa8d1ce0dfd1c72c5252848a7da571a22b5
SyK5 <Alpay.Sahin@outlook.de>
2024-10-16 13:21:13 +0200

    first commit creating directorys and creat scratch

---
+++ server/main.js
@@ -0,0 +1,21 @@
+import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import "dotenv/config";
+
+import connectDB from "./libs/database.js";
+
+await connectDB();
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(
+  cors({
+    origin: process.env.EXPRESS_CLIENT_URL,
+    credentials: true,
+  })
+);
+app.use(express.json());
+app.use(cookieParser());
+
+app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
