527013fd28a38d1c027b96387a2568472f630fbd
Rashma <lette2tania@gmail.com>
2024-10-16 13:41:51 +0200

    test commit for pushing

--- server/main.js
+++ server/main.js
@@ -7,7 +7,7 @@ import connectDB from "./libs/database.js";
 
 await connectDB();
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT ;
 
 app.use(
   cors({
