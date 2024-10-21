76de7f1487c3431c451993ceced852a9a53aabee
Preet3067 <harpreet1101as@gmail.com>
2024-10-21 16:13:45 +0200

    stage-1

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Category from './components/Category';
 import Vases from './components/Vases';
--- client/src/components/Category.jsx
+++ client/src/components/Category.jsx
@@ -1,5 +1,5 @@
 // client/src/components/Category.jsx
-import React, { useState } from 'react';
+import { useState } from 'react';
 import { Link } from 'react-router-dom';
 import './Category.css'; // Assuming styles for sliding effect
 
