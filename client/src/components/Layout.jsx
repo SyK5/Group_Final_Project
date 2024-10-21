0fd2b2aca3c72c2ac1318134b032e32115ed6345
SyK5 <Alpay.Sahin@outlook.de>
2024-10-21 15:05:56 +0200

    i try to get background with dot on the item what we wanna sell

--- client/src/components/Layout.jsx
+++ client/src/components/Layout.jsx
@@ -1,15 +1,14 @@
 import React, { useContext } from "react";
 import { Outlet } from "react-router-dom";
 // import Navbar from "../components/Navbar";
-import { MainContext } from "../contexts/Context_main";
+// import { MainContext } from "../contexts/Context_main";
 
 const Layout = () => {
-//   const { cartsState } = useContext(MainContext);
-
-//   const containerWidth = cartsState.isOpen ? "calc(100vw - 200px)" : "100%";
+  // const { cartsState } = useContext(MainContext);
+  // const containerWidth = cartsState.isOpen ? "calc(100vw - 200px)" : "100%";
 
   return (
-    <div className="app" style={{width: '10vw' }}>
+    <div className="app" style={{ width: '100vw', height: '100vh', overflow: 'hidden' }}>
       {/* <Navbar /> */}
       <Outlet />
     </div>
