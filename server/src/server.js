1669e22a3ac093e4648773aca405d3654fca703a
Preet3067 <harpreet1101as@gmail.com>
2024-10-22 15:33:43 +0200

    changes to serverjs

--- server/src/server.js
+++ server/src/server.js
@@ -7,11 +7,11 @@ import cors from 'cors';
 const app = express();
 const port = 3000;
 
-// Middleware
+
 app.use(cors({ origin: '*' }))
 app.use(express.json());
 
-// Routes
+
 app.use('/api/vases', vasesRoutes);
 
 app.listen(port, () => {
