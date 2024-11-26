d59b2714437939c36851d9a54c6d0da1c7256aa7
SyK5 <Alpay.Sahin@outlook.de>
2024-11-26 12:19:38 +0100

    adding little things

--- client/src/components/Menu.jsx
+++ client/src/components/Menu.jsx
@@ -1,8 +1,8 @@
 import { CiMenuBurger } from "react-icons/ci";
 import { useState } from "react";
-import "./Menu.css";
+import "./Menu.scss";
 import { Link, NavLink } from "react-router-dom";
-
+import { CiSearch } from "react-icons/ci";
 
 const Menu = () => {
   const [menuOpen, setMenuOpen] = useState(false);
@@ -11,19 +11,54 @@ const Menu = () => {
     setMenuOpen(!menuOpen);
   };
 
+    const [Searching, setSearching] = useState("Search");
+
   return (
     <div>
-      <div onMouseEnter={() => setMenuOpen(true)} onMouseLeave={() => setMenuOpen(false)} className="menu-button">
+      <div
+        onMouseEnter={() => setMenuOpen(true)}
+        onMouseLeave={() => setMenuOpen(false)}
+        className="menu-button"
+      >
         <CiMenuBurger />
       </div>
-      <div onMouseEnter={() => setMenuOpen(true)} className={`side-menu ${menuOpen ? "open" : ""}`}>
+      <div
+        onMouseLeave={() => setMenuOpen(false)}
+        onMouseEnter={() => setMenuOpen(true)}
+        className={`side-menu ${menuOpen ? "open" : ""}`}
+      >
         <button onClick={toggleMenu} className="close-button">
           X
         </button>
         <ul>
-        <NavLink to={"/home-owner"} className='HOwner'> Home Owner </NavLink>
-        <NavLink to={"/interior-designer"} className='IDesigner'> Interior Designer </NavLink>
-        <NavLink to={"/premium-member"} className='PMember'> Premium Member </NavLink>
+          <NavLink to={"/home-owner"} className="HOwner">
+            {" "}
+            Home Owner{" "}
+          </NavLink>
+          <NavLink to={"/interior-designer"} className="IDesigner">
+            {" "}
+            Interior Designer{" "}
+          </NavLink>
+          <NavLink to={"/premium-member"} className="PMember">
+            {" "}
+            Premium Member{" "}
+          </NavLink>
+          <NavLink to={"/Sales"} className="Sales">
+            {" "}
+            Sales{" "}
+          </NavLink>
+          <NavLink to={"/cart"} className="Cart">
+            {" "}
+            Cart{" "}
+          </NavLink>
+          <NavLink to={"/search"}
+            onMouseEnter={() => setSearching("Search-hover")}
+            onMouseLeave={() => setSearching("Search")}
+            className={Searching}
+          >
+            {" "}
+            <CiSearch /> search{" "}
+          </NavLink>
         </ul>
       </div>
     </div>
