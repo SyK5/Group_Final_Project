cc5a9be093d05d3326abb85ccf0b26219ff9283e
Alpay Sahin <153837671+SyK5@users.noreply.github.com>
2024-11-28 11:40:08 +0100

    Update User.js

--- server/model/User.js
+++ server/model/User.js
@@ -1,28 +1,10 @@
-import mongoose, { Schema } from "mongoose";
-import bcrypt from "bcryptjs";
-
-const UserSchema = new Schema({
-  firstName: { type: String, required: true },
-  lastName: { type: String, required: true },
-  username: { type: String, required: true, unique: true, trim: true },
-  gender: { type: String, enum: ["male", "female"], required: true },
-  email: { type: String, required: true, unique: true, trim: true },
-  password: { type: String, required: true },
-  premium: { type: Boolean, default: false },
-}, { timestamps: true });
-
-UserSchema.pre("save", async function(next) {
-  if (!this.isModified("password")) return next();
-  this.password = await bcrypt.hash(this.password, 10);
-  next();
-});
-
-UserSchema.methods.isPasswordCorrect = async function(candidatePassword) {
-  return await bcrypt.compare(candidatePassword, this.password);
-};
+import { Schema, model } from "mongoose";
 
 
-export default mongoose.model("User", UserSchema);
+const userSchema = new Schema ({
+    firstName: {
+        type: String,
+        required: true,
 
 
     },
